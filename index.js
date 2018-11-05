33c62520cd83a97ce2039904e97fac2374e8ad77
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-05 05:26:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function BoardMember(name, homeState, training) {
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+  this.veto = function() {
+    return console.log("No, I must disagree");
+  };
+  this.approve = function() {
+    return console.log("You can do that!");
+  };
+  this.doCharity = function() {
+    return console.log("I like to help people.");
+  };
+  this.releasePressStatement = function() {
+    return console.log("You will see great things from Scuber.");
+  };
+  this.sayHi = function() {
+    return console.log(`Hi, my name is ${this.name}, I am from ${this.homeState}, and I was trained in ${this.training}``)
+  }
+}
