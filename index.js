299c3e4ee655c5a0f59b907a070ca68e8af182b6
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-05 05:28:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function BoardMember(name, homeState, training) {
   this.homeState = homeState;
   this.training = training;
   this.veto = function(){
-    console.log("No, I must disagree")
+    console.log("No, I must disagree");
   }
   this.approve = function() {
     return console.log("You can do that!");
